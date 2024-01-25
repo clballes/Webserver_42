4f98ab0c3fdfd646fb1ffb1a27e92a76992e310e
puyma <mpuig-ma@student.42barcelona.com>
2024-01-25 14:31:27 +0100

    init test kqueue

---
+++ test-kqueue.cpp
@@ -0,0 +1,15 @@
+/*                                                                            */
+/* test-kqueue.cpp                                                            */
+/* mpuig-ma <mpuig-ma@student.42barcelona.com>                                */
+/* Thu Jan 25 14:26:11 2024                                                   */
+
+#include "webserv.hpp"
+
+int
+main (int argc, const char** argv)
+{
+	(void)argc;
+	(void)argv;
+
+	return (0x0);
+}
