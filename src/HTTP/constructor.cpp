7f455a10893b33bf3dcd51121fe4a17b93af7799
puyma <mpuig-ma@student.42barcelona.com>
2024-02-28 16:38:57 +0100

    HTTP class init

---
+++ src/HTTP/constructor.cpp
@@ -0,0 +1,13 @@
+/*                                                                            */
+/* constructor.cpp                                                            */
+/* mpuig-ma <mpuig-ma@student.42barcelona.com>                                */
+/* Wed Feb 28 16:18:46 2024                                                   */
+
+#include "HTTP.hpp"
+
+HTTP::HTTP ( )
+{
+	LOG( "call HTTP:HTTP()" );
+
+	return ;
+}
---
+++ src/HTTP/destructor.cpp
@@ -0,0 +1,13 @@
+/*                                                                            */
+/* destructor.cpp                                                             */
+/* mpuig-ma <mpuig-ma@student.42barcelona.com>                                */
+/* Wed Feb 28 16:19:12 2024                                                   */
+
+#include "HTTP.hpp"
+
+HTTP::~HTTP ( void )
+{
+	LOG( "call HTTP::~HTTP()" );
+
+	return ;
+}
