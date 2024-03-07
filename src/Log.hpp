2aef93b9d386fcbda911c71e55bd347417d8bd6a
puyma <mpuig-ma@student.42barcelona.com>
2024-03-07 15:06:31 +0100

    LOG_BUFFER

--- src/Log.hpp
+++ src/Log.hpp
@@ -8,3 +8,17 @@
 #include <iostream>
 
 #define LOG(s) { std::clog << s << std::endl; }
+
+#define LOG_BUFFER(str) \
+{ \
+	char *jakala = str; \
+	while ( jakala != 0 && *jakala != 0 ) \
+	{ \
+		if ( std::isprint( *jakala ) ) { std::clog << *jakala; } \
+		else if ( *jakala == 011 ) { std::clog << "\\t"; } \
+		else if ( *jakala == 012 ) { std::clog << "\\n"; } \
+		else if ( *jakala == 015 ) { std::clog << "\\r"; } \
+		++jakala; \
+	} \
+	std::clog << std::endl; \
+}
