1f9a9652a7c65c2adfadf9b031fd002f953a1cd9
puyma <mpuig-ma@student.42barcelona.com>
2024-03-20 18:12:37 +0100

    use p tags

--- tests/cgi_script.py
+++ tests/cgi_script.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
-import cgi
+import os
 
 print("Content-Type: text/html\n")  # Required header indicating HTML content
 
 print("<html><head><title>CGI Python Script</title></head><body>")
 print("<h1>Hello CGI World!</h1>")
 print("<p>This is a simple CGI Python script.</p>")
+
+for key, value in os.environ.items():
+    print(f'<p>{key}={value}</p>')
+
 print("</body></html>")
