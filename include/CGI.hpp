a47446540ea11ef6502e9917f7bea81e65472c7d
puyma <mpuig-ma@student.42barcelona.com>
2024-04-11 14:37:09 +0200

    rm Log.hpp

--- include/CGI.hpp
+++ include/CGI.hpp
@@ -3,14 +3,11 @@
 /* mpuig-ma <mpuig-ma@student.42barcelona.com>                                */
 /* Wed Mar 20 12:16:57 2024                                                   */
 
-
-
 #ifndef _WEBSERV_CGI_HPP_
 #define _WEBSERV_CGI_HPP_
 
 #include "IEvent.hpp"
 #include "HTTP.hpp"
-// #include "Log.hpp"
 
 class HTTP;
 
@@ -25,8 +22,8 @@ class CGI : public IEvent
 		int register_process( pid_t pid );
 		int execute ( void );
 
-
 	protected:
+
 		HTTP &  _http;
 		char ** _env;
 		int _pipefd[2];
