1577719197c1e33bbcd04ccb516a6adbedb5ea6f
puyma <mpuig-ma@student.42barcelona.com>
2024-05-09 13:11:56 +0200

    updated LOG system

--- Makefile
+++ Makefile
@@ -27,7 +27,8 @@ SRC_FILES		:=	$(SRC_DIR)/main.cpp \
 					$(SRC_DIR)/CGI.cpp \
 					$(SRC_DIR)/string.cpp \
 					$(SRC_DIR)/file.cpp \
-					$(SRC_DIR)/autoindex.cpp
+					$(SRC_DIR)/autoindex.cpp \
+					$(SRC_DIR)/log.cpp 
 
 OBJ_FILES		=	$(SRC_FILES:$(SRC_DIR)/%.cpp=$(BUILD_DIR)/%.o)
 DEP_FILES		=	$(SRC_FILES:$(SRC_DIR)/%.cpp=$(BUILD_DIR)/%.d)
--- include/log.hpp
+++ include/log.hpp
@@ -6,12 +6,9 @@
 #ifndef _LOG_HPP__
 #define _LOG_HPP__
 
-#include <iostream>
+#include <sys/cdefs.h>
 #include <ctime>
-
-// TODO: define DEBUG to nothing if not -DDEBUG
-// TODO: implement hierarchy INFO -> WARN -> ERROR
-// nginx's: debug | info | notice | warn | error | crit | alert | emerg
+#include <iostream>
 
 #ifdef PRETTY
 #define __FN__ __PRETTY_FUNCTION__
@@ -26,75 +23,41 @@
 #define RESET	"\033[0m"
 #define ENDL	"\n"
 
-#define DEBUG_TIME \
+#define _MODE_DEBUG		5
+#define _MODE_INFO		4
+#define _MODE_NOTICE	3
+#define _MODE_WARN		2
+#define _MODE_ERROR		1
+
+#define DEBUG(item)		_WEBSERV_LOG( _MODE_DEBUG, item )
+#define INFO(item)		_WEBSERV_LOG( _MODE_INFO, BLUE << item )
+#define NOTICE(item)	_WEBSERV_LOG( _MODE_NOTICE, YELLOW << item )
+#define WARN(item)		_WEBSERV_LOG( _MODE_WARN, YELLOW << item )
+#define ERROR(item)		_WEBSERV_LOG( _MODE_ERROR, RED << item )
+#define LOG(item)		{ std::clog << item << RESET << ENDL; }
+
+__BEGIN_DECLS
+extern int * __w_verbose_level(void);
+#define _webserv_verbose_level (*__w_verbose_level())
+__END_DECLS
+
+int _webserv_verbose_level;
+
+#define _WEBSERV_LOG_TIME \
 	char t[10]; \
 	std::time_t tt = std::time( 0x0 ); \
 	std::strftime( t, 10, "%H:%M:%S", std::localtime( &tt ) ); \
 	std::clog << "[" << t << "] ";
 
-#define DEBUG_TIME_EMPTY std::clog << "           ";
-
-// if not debug set should be:
-// #define DEBUG(item) ""
-
-#define DEBUG(item) \
+#define _WEBSERV_LOG(mode,item2log) \
 { \
-	DEBUG_TIME \
-	std::clog \
-	<< __FN__ \
-	<< " [" << item << "]" \
-	<< ENDL; \
+	int m = mode; \
+	if ( m <= _webserv_verbose_level ) \
+	{ \
+		_WEBSERV_LOG_TIME \
+		std::clog << item2log << RESET << ENDL; \
+	} \
 }
-
-#define ERROR_TIME(s) \
-{ \
-	DEBUG_TIME \
-	std::clog << s << ENDL; \
-}
-
-#define ERROR_EMPTY(s) \
-{ \
-	DEBUG_TIME_EMPTY \
-	std::clog << s << ENDL; \
-}
-
-#define ERROR(s) \
-{ \
-	std::clog << RED \
-	<< s << RESET << ENDL; \
-}
-
-#define INFO_TIME(s) \
-{ \
-	DEBUG_TIME \
-	std::clog << s << ENDL; \
-}
-
-#define INFO_EMPTY(s) \
-{ \
-	DEBUG_TIME_EMPTY \
-	std::clog << s << ENDL; \
-}
-
-#define INFO(s) \
-{ \
-	std::clog << s << ENDL; \
-}
-
-#define WARN(s) \
-{ \
-	std::clog << YELLOW \
-	<< s << RESET << ENDL; \
-}
-
-#define LOG(s) { std::clog << s << RESET << ENDL; }
-
-#define LOG_TIME(s) \
-{ \
-	DEBUG_TIME \
-	std::clog << s << RESET << ENDL; \
-}
-
 #define LOG_BUFFER(str,color) \
 { \
 	std::string::const_iterator it = str.begin(); \
