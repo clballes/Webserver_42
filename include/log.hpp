205880562d5c995e604e88cd6c114f03812c33ab
puyma <mpuig-ma@student.42barcelona.com>
2024-05-14 13:43:16 +0200

    rm DEBUG macro and unused files

--- include/log.hpp
+++ include/log.hpp
@@ -23,14 +23,12 @@
 #define RESET	"\033[0m"
 #define ENDL	"\n"
 
-#define _MODE_DEBUG		5
 #define _MODE_LOG		4
 #define _MODE_INFO		3
 #define _MODE_NOTICE	2
 #define _MODE_WARN		1
 #define _MODE_ERROR		0
 
-#define DEBUG(item)		_WEBSERV_LOG( _MODE_DEBUG, item )
 #define INFO(item)		_WEBSERV_LOG( _MODE_INFO, item )
 #define NOTICE(item)	_WEBSERV_LOG( _MODE_NOTICE, YELLOW << item )
 #define WARN(item)		_WEBSERV_LOG( _MODE_WARN, YELLOW << item )
@@ -56,30 +54,8 @@ int _webserv_verbose_level;
 	if ( m <= _webserv_verbose_level ) \
 	{ \
 		if ( m != _MODE_LOG ) { _WEBSERV_LOG_TIME } \
-		if ( m == _MODE_DEBUG ) \
-		{ std::clog << __FN__ << " [" << item2log << RESET << "]" << ENDL; } \
-		else { std::clog << item2log << RESET << ENDL; } \
+		std::clog << item2log << RESET << ENDL; \
 	} \
-}
-#define LOG_BUFFER(str,color) \
-{ \
-	std::string::const_iterator it = str.begin(); \
-	std::string::size_type counter = 1; \
-	while ( it != str.end() ) \
-	{ \
-		if ( it == str.begin() ) { std::clog << color; } \
-		if ( std::isprint( *it ) ) { std::clog << *it; } \
-		else if ( *it == '\t' ) { std::clog << "\\t"; ++counter; } \
-		else if ( *it == '\n' ) { std::clog << "\\n"; ++counter; } \
-		else if ( *it == '\v' ) { std::clog << "\\v"; ++counter; } \
-		else if ( *it == '\f' ) { std::clog << "\\f"; ++counter; } \
-		else if ( *it == '\r' ) { std::clog << "\\r"; ++counter; } \
-		/*if ( counter >= 80 ) { std::clog << ENDL; counter = 0; } */\
-		++it; \
-		++counter; (void) counter; \
-		if ( it == str.end() ) { std::clog << ENDL; } \
-	} \
-	std::clog << RESET; \
 }
 
 #endif /* !_LOG_HPP_ */
